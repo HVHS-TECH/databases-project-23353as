be7a2befa05edc5845960fb2d374bc7ec9e07f57
Aaron Shaji <23353as@hvhs.school.nz>
2026-06-02 12:29:20 +1200

    Connected database, tested database input

---
+++ fb_setup.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkmwPbDi6Si4UpVseVyfmL-Y9GT377uHU",
+    authDomain: "aaron-shaji-12comp-project.firebaseapp.com",
+    databaseURL: "https://aaron-shaji-12comp-project-default-rtdb.firebaseio.com",
+    projectId: "aaron-shaji-12comp-project",
+    storageBucket: "aaron-shaji-12comp-project.firebasestorage.app",
+    messagingSenderId: "539580933061",
+    appId: "1:539580933061:web:39a2252ad8ee90ad1321ac",
+    measurementId: "G-7CD6JKRFW8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
